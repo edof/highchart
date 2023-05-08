b41a7935eaec2ac4602013977e082e35d7bf120f
Edo Firmansyah <edoucreedthink@gmail.com>
2023-05-08 15:01:22 +0700

    add highchart-3d

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import highcharts3d from 'highcharts/highcharts-3d';
+highcharts3d(Highcharts);
+
+interface ExtendedPlotAreaOptions extends Highcharts.PlotAreaOptions {
+  depth: number; marker: { enabled: false; }; states: { inactive: { enabled: false; }; };
+}
 
 @Component({
   selector: 'app-home',
@@ -8,7 +14,10 @@ import * as Highcharts from 'highcharts';
 })
 export class HomePage implements OnInit{
   Highcharts2: typeof Highcharts = Highcharts;
+  Highcharts: typeof Highcharts = Highcharts;
+
   chartOptions2: Highcharts.Options = {}
+  chartOptions: Highcharts.Options = {}
   constructor() {}
 
   ngOnInit(): void {
@@ -75,6 +84,121 @@ export class HomePage implements OnInit{
         type: 'line'
       }]
     };
+
+    this.chartOptions =
+     {
+      chart: {
+        type: 'area',
+        options3d: {
+          enabled: true,
+          alpha: 15,
+          beta: 30,
+          depth: 200
+        }
+      },
+      title: {
+        text: 'RPM vs Speed'
+      },
+      // accessibility: {
+      //   description: 'The chart is showing the shapes of three mountain ranges as three area line series laid out in 3D behind each other.',
+      //   keyboardNavigation: {
+      //     seriesNavigation: {
+      //       mode: 'serialize'
+      //     }
+      //   }
+      // },
+      // lang: {
+      //   accessibility: {
+      //     axis: {
+      //       xAxisDescriptionPlural: 'The chart has 3 unlabelled X axes, one for each series.'
+      //     }
+      //   }
+      // },
+      yAxis: [{ //--- Primary yAxis
+        title: {
+            text: 'RPM'
+        }
+      }, { //--- Secondary yAxis
+        title: {
+            text: 'Speed'
+        },
+        opposite: true
+      }],
+      xAxis: {
+        categories: [
+          "01:02",
+          "01:05",
+          "01:09",
+          "01:16",
+          "01:18",
+          "01:18",
+          "01:08",
+          "01:11",
+          "01:17",
+          "01:20",
+
+      ]
+      },
+      plotOptions: {
+        area: {
+          depth: 100,
+          marker: {
+            enabled: false
+          },
+          states: {
+            inactive: {
+              enabled: false
+            }
+          }
+        } as ExtendedPlotAreaOptions
+      },
+      // tooltip: {
+      //   valueSuffix: ' MAMSL'
+      // },
+      series: [{
+        yAxis: 0,
+        name: 'RPM',
+        data: [
+          1264,
+          1009,
+          754,
+          840,
+          1163,
+          929,
+          1036,
+          1103,
+          1205,
+          1217,
+
+        ],
+        lineColor: 'rgb(180,90,50)',
+        color: 'rgb(200,110,50)',
+        fillColor: 'rgb(200,110,50)',
+        type: 'area',
+
+      }, {
+        yAxis: 1,
+        name: 'Speed',
+        data: [
+          38,
+          30,
+          22,
+          25,
+          47,
+          37,
+          31,
+          33,
+          49,
+          49,
+
+      ],
+        lineColor: 'rgb(120,160,180)',
+        color: 'rgb(140,180,200)',
+        fillColor: 'rgb(140,180,200)',
+        type: 'area',
+      }]
+    };
   }
+
 
 }
